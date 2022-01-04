f4ffb2206eec9515b4751dbc2cf8e23b88501881
Ogulcan <ogulcanturan10@gmail.com>
2022-01-05 00:09:34 +0300

    Singleton and Factory

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(new LoggerFactory());
+            CustomerManager customerManager2 = new CustomerManager(new LoggerFactory2());
+
+            customerManager.Save();
+            customerManager2.Save();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class LoggerFactory:ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new EdLogger();
+        }
+    }
+
+    public class LoggerFactory2 : ILoggerFactory
+    {
+        public ILogger CreateLogger()
+        {
+            //Business to decide factory
+            return new LoggedFNetLogger();
+        }
+    }
+    public interface ILoggerFactory
+    {
+        ILogger CreateLogger();
+    }
+
+    public interface ILogger
+    {
+        void Log();
+    }
+
+    public class EdLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("Logged with EdLogger");
+        }
+    }
+
+    public class LoggedFNetLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("LoggedFNetLogger with EdLogger");
+        }
+    }
+
+    public class CustomerManager
+    {
+        //Sürekli logger factory newlememek için
+
+        private ILoggerFactory _loggerFactory;
+
+        public CustomerManager(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+        public void Save()
+        {
+            Console.WriteLine("Saved!");
+            ILogger logger =  _loggerFactory.CreateLogger();
+            logger.Log();
+        }
+    }
+}
